19403d208e827abf127e0c69ba9eea396dc5bcc7
Anis Mojahid <anismojahid@hotmail.com>
2015-08-25 09:26:52 +0200

    Hoofdstuk 4, opdracht 3.

---
+++ src/h01/opdracht4_3.java
@@ -0,0 +1,29 @@
+package h01;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class opdracht4_3 extends Applet {
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public void init() {
+    setBackground(Color.white);
+  }
+
+  public void paint(Graphics g) {
+	  g.drawRect(40, 20, 50, 20);
+	  g.drawRect(40, 40, 50, 20);
+	  g.drawRect(40, 60, 50, 20);
+	  g.drawLine(40, 130, 40, 20);
+	  
+	  g.setColor(Color.red);
+	  g.fillRect(40, 20, 50, 20);
+	  g.setColor(Color.blue);
+	  g.fillRect(40, 60, 50, 20);
+  }
+} 
+    
